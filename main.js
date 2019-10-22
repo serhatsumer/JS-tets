8badf2c11f3713efcb41950c512996ec185f43a8
serhat <serhat-sumer@hotmail.com>
2019-10-22 23:31:15 +0200

    add test code

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');    
+} else {
+  alert('Awwww, but chocolate is my favorite...');    
+}
+
+
+function multiply(num1,num2) {
+    var result = num1 * num2;
+    return result;
+  }
+    
+  multiply(4,7);
+  multiply(20,20);
+  multiply(0.5,3)
+  
+  //Array literals
+  
+  var coffees = ['French Roast', 'Colombian', 'Kona'];
